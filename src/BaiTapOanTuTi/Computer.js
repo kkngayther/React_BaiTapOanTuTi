1f5a4586f0a057cf59ef18d1a3402b0db69b4136
kkngayther <leanhtrieu21.09.2k@gmail.com>
2020-12-13 11:08:48 +0700

    bai tap oan tu ti ver3.0

--- src/BaiTapOanTuTi/Computer.js
+++ src/BaiTapOanTuTi/Computer.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux'
+import styleGame from './BaiTapOanTuTi.module.css';
+import { connect } from 'react-redux'
 
 class Computer extends Component {
     render() {
         return (
             <div className="col-4">
                 <div style={{ display: 'grid', justifyContent: 'center' }}>
-                    <img width="180" height="180" style={{ background: 'white' }} src={this.props.computer.hinhAnh}/>
+                    <div className={`${styleGame.think}`}>
+                        <img width="180" height="180" style={{ background: 'white' }} src={this.props.computer.hinhAnh} />
+                    </div>
+                    <div className={`${styleGame.bubble}`}></div>
                     <img width="180" height="180" src="./img/imgOanTuTi/playerComputer.png" />
                 </div>
             </div>
--- src/BaiTapOanTuTi/Player.js
+++ src/BaiTapOanTuTi/Player.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import styleGame from './BaiTapOanTuTi.module.css';
 import { connect } from 'react-redux'
 
 class Player extends Component {
     renderOanTuTi = () => {
         return this.props.selection.map((select, index) => {
+            let border = {};
+            if (select.datCuoc){
+                border = {border: '3px solid orange'};
+            }
             return <div>
-                <button className="mr-3" onClick={() => {
+                <button style={border} className="mr-3" onClick={() => {
                     this.props.dispatch({
                         type: 'CHANGE_SELECTION',
                         maCuoc: select.ma
                     });
                 }} >
-                    <img key={index} width="50" height="50" src={select.hinhAnh} style={{ background: 'white' }} alt={select.hinhAnh}/>
+                    <img key={index} width="50" height="50" src={select.hinhAnh} style={{ background: 'white' }} alt={select.hinhAnh} />
                 </button>
             </div>
         })
@@ -20,7 +25,10 @@ class Player extends Component {
         return (
             <div className="col-4">
                 <div style={{ display: 'grid', justifyContent: 'center' }}>
-                    <img width="180" height="180" style={{ background: 'white' }} src={this.props.selection.find(item => item.datCuoc).hinhAnh} />
+                    <div className={`${styleGame.think}`}>
+                        <img width="180" height="180" style={{ background: 'white' }} src={this.props.selection.find(item => item.datCuoc).hinhAnh} />
+                    </div>
+                    <div className={`${styleGame.bubble}`}></div>
                     <img width="180" height="180" src="./img/imgOanTuTi/player.png" />
                 </div>
                 <div className="d-flex justify-content-center">
